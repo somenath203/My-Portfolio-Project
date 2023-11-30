32c09b69d950063c85c30960be71815023dfa328
Somenath Choudhury <81456073+somenath203@users.noreply.github.com>
2023-11-30 18:17:43 +0530

    Update AllMlProjects.js

--- src/pages/AllMlProjects.js
+++ src/pages/AllMlProjects.js
@@ -193,7 +193,7 @@ const AllMlProjects = () => {
         },
         {
             id: 15,
-            projectName: 'Text Summarizer using Transformer',
+            projectName: 'Text Summarizer using Transformers',
             projectDescription: 'This is a application which is created with the help of Gradio whose work is to summarize text articles with the help of \'Summarization Pipeline\' provided by Transformer.',
             projectPic: SummarizeText,
             heightOfImg: 'h-60',
